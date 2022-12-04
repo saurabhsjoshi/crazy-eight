3180c399b3a1c52348c16b0f20d0ea89991a6847
Saurabh Joshi <saurabhsjoshi94@gmail.com>
2022-12-04 11:25:48 -0500

    [NETWORKING] Add code for handling user registration via websocket
    Signed-off-by: Saurabh Joshi <saurabhsjoshi94@gmail.com>

--- backend/src/main/java/org/joshi/crazyeight/network/WebSocketMsgHandler.java
+++ backend/src/main/java/org/joshi/crazyeight/network/WebSocketMsgHandler.java
@@ -1,6 +1,8 @@
 package org.joshi.crazyeight.network;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.joshi.crazyeight.game.Game;
 import org.joshi.crazyeight.msg.UserRegisterMsg;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
@@ -8,12 +10,23 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * This class handles all messages received by the websocket.
  */
 @Component
+@Slf4j
 public class WebSocketMsgHandler extends TextWebSocketHandler {
     private final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final ConcurrentHashMap<String, WebSocketSession> socketHandles = new ConcurrentHashMap<>();
+
+    private final Game game;
+
+    public WebSocketMsgHandler() {
+        this.game = new Game();
+    }
 
     @Override
     public void afterConnectionEstablished(@NonNull WebSocketSession session) {
@@ -24,9 +37,15 @@ public class WebSocketMsgHandler extends TextWebSocketHandler {
     protected void handleTextMessage(@NonNull WebSocketSession session, TextMessage message) throws Exception {
         var msg = objectMapper.readValue(message.getPayload(), Message.class);
         if (msg instanceof UserRegisterMsg registerMsg) {
-            System.out.println("User registered with username " + registerMsg.username);
+            handleUserRegister(registerMsg, session);
         }
-        System.out.println("Received message " + message.getPayload());
+    }
+
+    private void handleUserRegister(UserRegisterMsg registerMsg, WebSocketSession session) {
+        String username = registerMsg.username;
+        game.addPlayer(username);
+        socketHandles.put(username, session);
+        log.info("Registered user '{}'.", registerMsg.username);
     }
 
 }
